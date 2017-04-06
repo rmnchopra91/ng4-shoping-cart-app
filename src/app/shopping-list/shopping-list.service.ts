077d74cf699dcf7b8c2ef5cd44996af95ad7db84
raman <raman.chopra@agilenova.com>
2017-04-06 18:59:33 +0530

    complete Service and dependency Injection

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,6 +1,9 @@
+import { EventEmitter } from '@angular/core';
+
 import { Ingredient } from '../shared/ingredient.model';
 
 export class ShopingListService {
+    ingredientsChanged = new EventEmitter<Ingredient[]>();
     private ingredients: Ingredient[]= [
         new Ingredient('Apple',5),
         new Ingredient('Tomatoes',10)
@@ -10,5 +13,13 @@ export class ShopingListService {
     }
     addIngredient(ingredient: Ingredient){
         this.ingredients.push(ingredient);
+        this.ingredientsChanged.emit(this.ingredients.slice());
+    }
+    addIngredients(ingredients: Ingredient[]){
+        // for(let ingredient of ingredients){
+        //     this.addIngredient(ingredient);
+        // }
+        this.ingredients.push(...ingredients);
+        this.ingredientsChanged.emit(this.ingredients.slice());
     }
 }
